a7541cf7a927cca68bc5df34b9e532d753748e1b
Laaouina18 <laaouinanouhaila2019@gmail.com>
2024-03-27 15:00:01 +0000

    Ajouter les fichiers backend nécessaires pour le projet

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+
+const generateToken = (user) => {
+  const payload = {
+    userId: user._id,
+    username: user.firstName,
+	role:user.role
+  };
+
+  const options = {
+  expiresIn: '2h',
+  };
+
+ const token = jwt.sign(payload, process.env.JWT_SECRET, options);
+   return token;
+};
+
+export { generateToken };
